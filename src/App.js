a01a3f4fca4a033d5f3ff106d1c36c96e57b28ec
Thomas Kwashnak <thomaskwashnak@gmail.com>
2022-10-13 11:34:07 -0400

    Misc. minor edit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles/App.scss";
 
 const App = () => {
-  
-
-  return <div id="App">I am not a bird</div>;
+  return (
+    <>
+      <div>Am I a bird?</div>
+    </>
+  );
 };
 
 export default App;
